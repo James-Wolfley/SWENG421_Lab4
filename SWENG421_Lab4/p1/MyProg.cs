2343cb06b15b1d5d66cc0ce9286caa4fdc1eb76f
jwolfley <james.wolfley@proton.me>
2026-02-04 23:36:41 -0500

    realized the prof lets us use the built in quicksort so I changed it

--- SWENG421_Lab4/p1/MyProg.cs
+++ SWENG421_Lab4/p1/MyProg.cs
@@ -10,28 +10,26 @@ public class MyProg {
         xyz.sortUtility = new BubbleSortUtility<ProductIF>();
         xyz.sortUtility.print(
             xyz.sortUtility.sort(
-                new List<ProductIF>
-                {
+                [
                     new Desk(1, "Writing Desk",  20.30),
                     new Desk(2, "Corner Desk",   15.25),
                     new Desk(3, "Lap Desk",      25.13),
                     new Desk(4, "Standing Desk", 15.85),
-                    new Desk(5, "Floating Desk", 22.56),
-                }
+                    new Desk(5, "Floating Desk", 22.56)
+                ]
             )
         );
 
         xyz.sortUtility = new QuickSortUtility<ProductIF>();
         xyz.sortUtility.print(
             xyz.sortUtility.sort(
-                new List<ProductIF>
-                {
+                [
                     new Desk(1, "Writing Desk",  20.30),
                     new Desk(2, "Corner Desk",   15.25),
                     new Desk(3, "Lap Desk",      25.13),
                     new Desk(4, "Standing Desk", 15.85),
-                    new Desk(5, "Floating Desk", 22.56),
-                }
+                    new Desk(5, "Floating Desk", 22.56)
+                ]
             )
         );
     }
